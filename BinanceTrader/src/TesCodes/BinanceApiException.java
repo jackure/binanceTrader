4025d0cb4990cb153a3100843d10007cb8b4627c
Jacint <jacint.ma@gmail.com>
2018-06-22 14:23:02 +0200

    frst cmmt

---
+++ BinanceTrader/src/TesCodes/BinanceApiException.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TesCodes;
+
+/* ============================================================
+ * java-binance-api
+ * https://github.com/webcerebrium/java-binance-api
+ * ============================================================
+ * Copyright 2017-, Viktor Lopata, Web Cerebrium OÜ
+ * Released under the MIT License
+ * ============================================================ */
+
+ public class BinanceApiException extends Throwable {
+     public BinanceApiException(String message) {
+         super(message);
+     }
+ }
